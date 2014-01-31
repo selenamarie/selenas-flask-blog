8a3ae5769b67296a942dd1d527fb65d0bebc4a27
Selena Deckelmann <selenamarie@gmail.com>
2014-01-31 15:48:41 -0800

    static page generator compleat

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is some simple blog software to replace my wordpress site.
+
+made with lots of help from https://nicolas.perriault.net/code/2012/dead-easy-yet-powerful-static-website-generator-with-flask/
--- sitebuilder.py
+++ sitebuilder.py
@@ -1,5 +1,10 @@
-from flask import Flask
+#! /usr/bin/env pythnon
+
+import sys
+
+from flask import Flask, render_template
 from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -9,13 +14,24 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
 
+freezer = Freezer(app)
+
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html', pages=pages)
+
+@app.route('/tag/<string:tag>/')
+def tag(tag):
+    tagged = [p for p in pages if tag in p.meta.get('tags', [])]
+    return render_template('tag.html', pages=tagged, tag=tag)
 
 @app.route('/<path:path>/')
 def page(path):
-    return pages.get_or_404(path).html
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
