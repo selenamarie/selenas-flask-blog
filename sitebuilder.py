66fc2d1ef57a05ed9af9a7a4445475c2840c874f
Selena Deckelmann <selenamarie@gmail.com>
2014-02-01 11:23:25 -0800

    Added requests import

--- sitebuilder.py
+++ sitebuilder.py
@@ -3,7 +3,7 @@
 import sys
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
